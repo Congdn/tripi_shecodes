d21961d621b9e400440ddb1b58339e53d849b89e
Công Ngô <darkking1999hy@outlook.com>
2020-08-18 21:54:00 +0700

    f

--- screens.js
+++
@@ -1,7 +0,0 @@
-import LoginScreen from './components/login/loginScreen';
-import HomeScreen from './components/home/homeScreen';
-import HotelScreen from './components/hotel/hotelScreen';
-import PaymentScreen from './components/payment/paymentScreen';
-import UserScreen from './components/user/userScreen';
-
-export default { LoginScreen, HomeScreen, HotelScreen, PaymentScreen, UserScreen };
